1d7be1f56136b292e0f36e8ae7508e0ca62706dc
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2024-12-06 14:54:27 +0530

    day2

---
+++ day2/index.js
@@ -0,0 +1,22 @@
+// *****
+// *
+// *
+// *
+// *****
+
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+// revision
+// spread work array and object
+
+
+let array = [1, 2, 3, 4];
+
+let array2 = array;
+console.log(array2)
+console.log(array == array2);
